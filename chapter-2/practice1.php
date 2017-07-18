3650b240a36ee36bdc463e2359dc5c1de04dd106
RyoheiKamo <ryohei.kamo.p4@gmail.com>
2017-07-18 19:33:09 +0900

    第2章演習問題

---
+++ chapter-2/practice1.php
@@ -0,0 +1,17 @@
+<?php
+// Here your code !
+
+/*氏名の定義
+苗字と名前
+*/
+$first_name = 'Ryohei';
+$last_name = 'Kamo';
+
+$my_name = $first_name . ' '.$last_name;
+
+//氏名の出力
+echo($my_name);
+echo "\n";
+//文字数のチェック
+echo strlen($my_name);
+?>
