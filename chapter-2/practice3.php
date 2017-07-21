3b381372aa60e1a401cd9b88032e32ed3cb1265e
RyoheiKamo <ryohei.kamo.p4@gmail.com>
2017-07-21 11:58:40 +0900

    演習問題のナンバリングが間違えていたために修正しました

--- chapter-2/practice3.php
+++ chapter-2/practice3.php
@@ -1,22 +1,16 @@
 <?php
+// Here your code !
 
-//それぞれの品目の値段
-$hamburger = 4.95;
-$shake = 1.95;
-$cola = 0.85;
+/*氏名の定義
+苗字と名前
+*/
+$first_name = 'Ryohei';
+$last_name = 'Kamo';
 
-//チップと消費税のレート
-$tip_rate = 0.16;
-$tax_rate = 0.075;
+$my_name = $first_name . ' ' . $last_name;
 
-//商品のみの値段
-$meal = (2 * $hamburger) + $shake + $cola;
-
-//チップと消費税で支払う金額
-$tip = $meal * $tip_rate;
-$tax = $meal * $tax_rate;
-
-//合計
-$total = $meal + $tip + $tax;
-
-echo 'The total cost of the meal is $' . $total;
+//氏名の出力
+echo $my_name;
+echo "\n";
+//文字数のチェック
+echo strlen($my_name);
