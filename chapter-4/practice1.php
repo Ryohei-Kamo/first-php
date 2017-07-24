3de2d610ce4654e4aa20dac4324e436e53d1fc95
RyoheiKamo <ryohei.kamo.p4@gmail.com>
2017-07-24 18:38:03 +0900

    演習問題１sakusei

--- chapter-4/practice1.php
+++ chapter-4/practice1.php
@@ -1,1 +1,18 @@
 <?php
+
+$census = ['New York, NY' => 8175133,
+	'Las Angels, CA' => 3792621,
+	'Chicago, IL' => 2695598,
+	'Houston, TX' => 2100263,
+	'Philadelphia, PA' => 1526006,
+	'Phoenix, AZ' => 1445632,
+	'San Antonio, TX' => 1327407,
+	'San Diego, CA' => 1307402,
+	'Dallas, TX' => 1197816,
+	'San Jose, CA' => 945942,];
+
+$total = 0;
+foreach ($census as $city => $population){
+	$total += $population;
+	echo $city . "/n"; $population . "/n";
+}
