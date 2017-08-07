8df23eae5f1873d934eda62cc4f365936b946440
RyoheiKamo <ryohei.kamo.p4@gmail.com>
2017-08-07 18:25:03 +0900

    回答

---
+++ chapter-6/practice1.php
@@ -0,0 +1,19 @@
+<?php
+
+class Ingredient{
+    public $name;
+    public $cost;
+
+    public function __construct($name, $cost){
+    	$this->name = $name;
+    	$this->cost = $cost;
+    }
+
+    public function getName(){
+    	return $this->name;
+    }
+
+    public function getCost(){
+    	return $this->cost;
+    }
+}
---
+++ chapter-6/practice2.php
@@ -0,0 +1,24 @@
+<?php
+
+class Ingredient{
+	public $name;
+	public $cost;
+
+	public function __construct($name, $cost){
+		$this->name = $name;
+		$this->cost = $cost;
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+
+	public function getCost(){
+		return $this->cost;
+	}
+
+	public function setCost($cost){
+		$this->cost = $cost;
+	}
+}
+
